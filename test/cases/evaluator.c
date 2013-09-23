31c391d390bf0a41af0cec2d846ede0f26c77eec
Alexandre Araujo Moreira <alexandream@gmail.com>
2013-09-23 13:27:51 -0300

    This version finally makes the machine able to compute things that are non-trivial, by adding support to conditional statements and jumps. Although our capabilities are still very limited, now we can, for instance, write a simple number adder by using the primitives we already had on the previous version.
    The first instruction introduced is the _jump_. It's a simple
    instruction: It receives an offset relative to the current code
    pointer indicating where the computation should continue. After its
    execution and change of code pointer, no extra increment is added to
    the code pointer.
    
    The way it is encoded is as follows:
    1) it is a single-word instruction;
    2) it takes a single 24bits unsigned int argument encoded with the
       last 3 bytes of the instruction (other than the opcode);
    
    And it works by following the strategy below:
    1) decode the real offset. This is a signed integer obtained by
       decoding the 24 bits signed integer and extending signal bit
       to the left as necessary;
    2) the real offset computed in (1) is then added to the current code
       pointer.
    
    The second and third instructions introduced are _jump-if_ and
    _jump-unless_. Those receive a local and a 16bits signed integer as
    arguments and use the integer as an offset just like _jump_, but
    jumping only in case the value stored in the local is, respectively,
    different from boolean false or equal to boolean false.
    
    Note that, while _jump_ uses a biased signed integer that has to be
    decoded, _jump-if_ and _jump-unless_ both receive its argument already
    signed, so there's no need for further decoding.
    
    For now the machine has no instruction able to jump the entirety of
    the available code segment for the module (which can use address of
    up to 32bits), but that's not really a problem since we're not
    supposed to be jumping in and out of procedures. For its current
    purposes I doubt we'll ever have functions spanning more than 80
    million instruction-words. Conditional jumps have an even smaller
    range, but in case bigger range is needed we can always trampoline a
    conditional jump right through to an unconditional jump.
    
    At the end of this version we have three new instructions to our
    evaluator:
    
    1) jump: takes a 24 bits signed number argument (offset) and adds
             that offset to the current code pointer;
    2) jump-if: takes a local (condition) and a 16 bits signed number
                argument (offset) and adds the offset to the current
                code pointer if the value stored at condition is
                different from boolean false;
    3) jump-unless: takes a local (condition) and a 16 bits signed
                    number argument (offset) and adds the offset to the
                    current code pointer if the value stored at
                    condition is equal to boolean false;

--- test/cases/evaluator.c
+++ test/cases/evaluator.c
@@ -2,7 +2,8 @@
 #include "../test-case.h"
 
 #include "evaluator.h"
-#include "module.h"
+#include "module.i.h"
+#include "library.h"
 #include "types/primitive.h"
 #include "types/procedure.h"
 #include "nuvm.h"
@@ -13,17 +14,12 @@ nuvm_value_t return_input(void* input, nuvm_value_t dummy) {
 }
 
 BEGIN_TEST(test_evaluator_runs_simple_func) {
-//	nuvm_primitive_t* primitive_func =
-//		nuvm_new_primitive(return_input, (void*) 0xDEADBEEF);
-//
-//	nuvm_value_t prim = nuvm_wrap_pointer(primitive_func);
 	nuvm_value_t dummy;
 	dummy = nuvm_wrap_pointer(&dummy);
-	
+
 	nuvm_module_builder_t* builder = nuvm_new_module_builder();
 	uint32_t ientry = nuvm_module_builder_push_register(builder, dummy);
-//	uint32_t iprimitive = nuvm_module_builder_push_register(builder, prim);
-	uint32_t icode = 
+	uint32_t icode =
 		nuvm_module_builder_push_instruction(builder,
 		                                     nuvm_op_global_ref(0, ientry));
 
@@ -31,11 +27,11 @@ BEGIN_TEST(test_evaluator_runs_simple_func) {
 
 	nuvm_module_t* mod = nuvm_module_builder_build(builder, ientry);
 	nuvm_destroy_module_builder(builder);
-	
+
 	nuvm_procedure_t* proc = nuvm_new_procedure(mod, 0, 1);
 	nuvm_module_store_register(mod, ientry, nuvm_wrap_pointer(proc));
 	nuvm_evaluator_t* eval = nuvm_new_evaluator();
-	
+
 	nuvm_evaluator_setup(eval, mod);
 
 	nuvm_value_t ret = nuvm_evaluator_run(eval);
@@ -54,11 +50,11 @@ BEGIN_TEST(test_evaluator_runs_primitive_func) {
 	nuvm_value_t prim = nuvm_wrap_pointer(primitive_func);
 	nuvm_value_t dummy;
 	dummy = nuvm_wrap_pointer(&dummy);
-	
+
 	nuvm_module_builder_t* builder = nuvm_new_module_builder();
 	uint16_t iprimitive = nuvm_module_builder_push_register(builder, prim);
 	uint16_t ientry = nuvm_module_builder_push_register(builder, dummy);
-	uint32_t icode = 
+	uint32_t icode =
 		nuvm_module_builder_push_instruction(builder,
 		                                     nuvm_op_global_ref(1, iprimitive));
 	nuvm_module_builder_push_instruction(builder, nuvm_op_sva_call(0, 1, 1));
@@ -66,7 +62,7 @@ BEGIN_TEST(test_evaluator_runs_primitive_func) {
 
 	nuvm_module_t* mod = nuvm_module_builder_build(builder, ientry);
 	nuvm_destroy_module_builder(builder);
-	
+
 	nuvm_procedure_t* proc = nuvm_new_procedure(mod, 0, 2);
 	nuvm_module_store_register(mod, ientry, nuvm_wrap_pointer(proc));
 	nuvm_evaluator_t* eval = nuvm_new_evaluator();
@@ -79,3 +75,62 @@ BEGIN_TEST(test_evaluator_runs_primitive_func) {
 	fail_if((uint64_t)nuvm_unwrap_pointer(ret) != 0xDEADBEEF);
 } END_TEST
 
+nuvm_value_t _simple_adder(nuvm_evaluator_t* eval,
+                                   uint16_t a,
+                                   uint16_t b) {
+	static nuvm_primitive_t* pinc = NULL;
+	static nuvm_primitive_t* pzerop = NULL;
+	static nuvm_primitive_t* pdec = NULL;
+	if (pinc == NULL)
+		pinc = nuvm_new_primitive(nuvm_primitive_successor, NULL);
+	if (pzerop == NULL)
+		pzerop = nuvm_new_primitive(nuvm_primitive_is_zero, NULL);
+	if (pdec == NULL)
+		pdec = nuvm_new_primitive(nuvm_primitive_predecessor, NULL);
+	nuvm_value_t inc = nuvm_wrap_pointer(pinc);
+	nuvm_value_t zerop = nuvm_wrap_pointer(pzerop);
+	nuvm_value_t dec = nuvm_wrap_pointer(pdec);
+
+	nuvm_module_t* mod = nuvm_new_blank_module(6, 20);
+
+	register_pool_t* regpool = &mod->registers;
+	code_segment_t*  codeseg = &mod->code_segment;
+
+	register_pool_t_set(regpool, 0, inc);
+	register_pool_t_set(regpool, 1, dec);
+	register_pool_t_set(regpool, 2, zerop);
+	register_pool_t_set(regpool, 3, nuvm_wrap_fixnum((int32_t) a));
+	register_pool_t_set(regpool, 4, nuvm_wrap_fixnum((int32_t) b));
+
+#define CODE(INDEX, VALUE) code_segment_t_set(codeseg, INDEX, VALUE)
+	CODE( 0, nuvm_op_global_ref(0, 0)); // inc
+	CODE( 1, nuvm_op_global_ref(1, 1)); // dec
+	CODE( 2, nuvm_op_global_ref(2, 2)); // zerop
+	CODE( 3, nuvm_op_global_ref(3, 3)); // a
+	CODE( 4, nuvm_op_global_ref(4, 4)); // b
+	CODE( 5, nuvm_op_sva_call(5, 2, 3)); // sva-call acc zerop a
+	CODE( 6, nuvm_op_jump_if(5, +4));
+	CODE( 7, nuvm_op_sva_call(3, 1, 3));
+	CODE( 8, nuvm_op_sva_call(4, 0, 4));
+	CODE( 9, nuvm_op_jump(-4));
+	CODE(10, nuvm_op_return(4));
+#undef CODE
+
+	nuvm_value_t proc = nuvm_wrap_pointer(nuvm_new_procedure(mod, 0, 6));
+	register_pool_t_set(regpool, 5, proc);
+	mod->entry_point = 5;
+	nuvm_evaluator_setup(eval, mod);
+	nuvm_value_t result = nuvm_evaluator_run(eval);
+	nuvm_destroy_module(mod);
+	return result;
+}
+
+BEGIN_TEST(test_evaluator_runs_simple_adder) {
+	nuvm_evaluator_t* eval = nuvm_new_evaluator();
+
+	nuvm_value_t result;
+	result = _simple_adder(eval, 2, 2);
+
+	fail_unless(nuvm_is_fixnum(result));
+	fail_unless(nuvm_unwrap_fixnum(result) == 4);
+} END_TEST
