cdf7861442a2f94b92c2f8cb3a1108a16264b6ad
Alexandre Araujo Moreira <alexandream@gmail.com>
2013-12-21 23:25:41 -0200

    Created type for bundles.

--- src/nuvm.c
+++ src/nuvm.c
@@ -1,13 +1,17 @@
 #include "nuvm.h"
 #include "symbol-pool.h"
 
+#include "objects/bundles.h"
 #include "objects/primitives.h"
 #include "objects/procedures.h"
 #include "objects/strings.h"
 
+
 void
 n_init() {
 	n_init_values();
+
+	n_init_bundles();
 	n_init_primitives();
 	n_init_procedures();
 	n_init_strings();
