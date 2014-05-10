e336db4d9434048e67c7f987dcabb27fb907eeb8
Alexandre Araujo Moreira <alexandream@gmail.com>
2014-05-10 17:09:26 -0300

    Restructuring the basic format for modules.

--- common/raw-modules.h
+++ common/raw-modules.h
@@ -6,33 +6,38 @@
 #define MAGIC_MASK   0x000000FFFFFFFFFF
 #define MAGIC_NUMBER 0x0000004D56554E1B
 
-
 typedef struct n_raw_module_t n_raw_module_t;
+typedef struct n_module_descriptor_t n_module_descriptor_t;
 typedef struct n_constant_entry_t n_constant_entry_t;
 
-struct n_constant_entry_t {
-	uint8_t type_id;
-	uint32_t data_index;
-} __attribute__((packed));
 
 struct n_raw_module_t {
-	uint64_t header;
-	uint64_t host_descriptor;
-	
-	uint32_t data_size;
-	uint32_t code_size;
-	uint64_t extra_size;
-
-	uint16_t constants_count;
-	uint16_t globals_count;
-	
-	uint32_t _padding;
+	n_module_descriptor_t *descriptor;
 
 	n_constant_entry_t *constants_index;
 
 	uint8_t *data_segment;
 	uint8_t *code_segment;
 	uint8_t *extra_segment;
+};
+
+
+struct n_module_descriptor_t {
+	uint64_t header;
+	uint64_t host_descriptor;
+
+	uint32_t data_size;
+	uint32_t code_size;
+	uint64_t extra_size;
+
+	uint16_t constants_count;
+	uint16_t globals_count;
+} __attribute__((packed));
+
+
+struct n_constant_entry_t {
+	uint8_t type_id;
+	uint32_t data_index;
 } __attribute__((packed));
 
 
