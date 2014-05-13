0617fdd4b4bf53ef99b2dface706bf9b8d87ed8a
Alexandre Araujo Moreira <alexandream@gmail.com>
2014-05-13 05:23:07 -0300

    Minor test result presentation tweaks

--- tests/runner.c
+++ tests/runner.c
@@ -32,24 +32,32 @@ ATResultList* at_run_all_tests(char* test_name) {
 	return result_list;
 }
 
-void _print_results(ATResultList* result_list, bool show_failures) {
+void _print_results(ATResultList* result_list, bool show_failure_msgs,
+                                               bool show_success) {
 	int result_count = at_count_results(result_list);
 	int i, j;
+	int printed_count = 0;
 	for(i = 0; i < result_count; i++) {
 		ATResult* result = at_get_nth_result(result_list, i);
 		int failure_count = at_count_failures(result);
-		fprintf(stderr, "[%s]: %s\n",
-				(failure_count)?"F":"S",
-				at_get_full_name(result));
-		if (show_failures) {
-			for (j = 0; j < failure_count; j++) {
-				ATFailure* failure = at_get_nth_failure(result, j);
-				fprintf(stderr, "     %s[%d]: %s\n\n",
-						failure->file_name,
-						failure->line_number,
-						failure->message);
+		if (failure_count > 0 || show_success) {
+			printed_count++;
+			fprintf(stderr, "[%s]: %s\n",
+					(failure_count)?"F":"S",
+					at_get_full_name(result));
+			if (show_failure_msgs) {
+				for (j = 0; j < failure_count; j++) {
+					ATFailure* failure = at_get_nth_failure(result, j);
+					fprintf(stderr, "     %s[%d]: %s\n\n",
+							failure->file_name,
+							failure->line_number,
+							failure->message);
+				}
 			}
 		}
+	}
+	if (printed_count > 0) {
+		puts("");
 	}
 }
 
@@ -71,11 +79,11 @@ void _count_results(ATResultList* list, int* success, int* failure) {
 }
 
 void print_compact_results(ATResultList* result_list) {
-	_print_results(result_list, false);
+	_print_results(result_list, false, false);
 }
 
 void print_results(ATResultList* result_list) {
-	_print_results(result_list, true);
+	_print_results(result_list, true, false);
 }
 
 void print_result_totals(ATResultList* result_list) {
@@ -91,10 +99,7 @@ int main(int argc, char** argv) {
 
 	result_list = at_run_all_tests(test_name);
 
-	if (at_count_results(result_list) > 0) {
-		print_results(result_list);
-		puts("");
-	}
+	print_results(result_list);
 	print_result_totals(result_list);
 
 	return 0;
