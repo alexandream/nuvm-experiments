8d5ea617e09932e9b15af5ae083de869ce0d1092
Alexandre Araujo Moreira <alexandream@gmail.com>
2014-07-12 11:19:53 -0300

    New common asserts for tests.
    Two new test asserts were created: ASSERT_EOK(NError* error) and
    ASSERT_EQ_I64(int64_t, int64_t).
    
    ASSERT_EOK takes the error argument and checks that it is an "Ok" status
    -- that is, an error of type net.xndk.nuvm.erros.Ok
    
    ASSERT_EQ_I64 takes two 64 bits integers and checks that both are equal.
    
    Both assertions print suitable messages upon failure.

--- tests/suites/asm/reader.c
+++ tests/suites/asm/reader.c
@@ -7,138 +7,34 @@
 #include "lexer.h"
 #include "streams.h"
 
-uint8_t*
-n_asm_build_file(const char* fname) {
-	NStream* stream = ni_new_stream_from_path(fname);
-	NLexer* lexer = ni_new_lexer(stream);
-	
-	uint8_t major, minor, revision;
-	uint16_t num_globals, entry_point;
+static NLexer* LEXER = NULL;
 
-	NTokenType next_token;
+static NLexer*
+WITH_INPUT(const char* input);
 
-	NError error = { 0, };
-	
-	ni_read_version(lexer, &major, &minor, &revision, &error);
-	if (!n_error_ok(&error)) goto handle_error;
-	/* TODO: Set version on the builder. */
 
-	ni_read_entry_point(lexer, &entry_point, &error);
-	if (!n_error_ok(&error)) goto handle_error;
-	/* TODO: Set entry point on the builder. */
-
-	ni_read_globals_count(lexer, &num_globals, &error);
-	if (!n_error_ok(&error)) goto handle_error;
-	/* TODO: Set globals count on the builder. */
-
-	next_token = ni_lexer_peek(lexer);
-	if (next_token != NI_TK_KW_CONSTANTS) {
-		/* TODO: Create error, expected '.constants', found something else. */
+TEARDOWN {
+	if (LEXER != NULL) {
+		ni_destroy_lexer(LEXER);
+		LEXER = NULL;
 	}
-	{
-
-		double const_double_val;
-		char* const_string_val;
-		char* const_proc_label_val;
-		uint16_t const_proc_num_locals_val;
-		int32_t const_int32_val;
-
-		next_token = ni_lexer_peek(lexer);
-		while (next_token != NI_TK_KW_CODE) {
-			const_string_val = NULL;
-			const_proc_label_val = NULL;
-
-			switch(next_token) {
-				case NI_TK_KW_STRING:
-					ni_read_string_constant(lexer, &const_string_val, &error);
-					if (!n_error_ok(&error)) goto handle_error;
-					/* TODO: Do something with the string. */
-					free(const_string_val);
-					break;
-				case NI_TK_KW_DOUBLE:
-					ni_read_double_constant(lexer, &const_double_val, &error);
-					if (!n_error_ok(&error)) goto handle_error;
-					/* TODO: Do something with the double. */
-					break;
-				case NI_TK_KW_CHARACTER:
-					ni_read_character_constant(lexer,
-					                           &const_string_val,
-					                           &error);
-					if (!n_error_ok(&error)) goto handle_error;
-					/* TODO: Do something with the character. */
-					free(const_string_val);
-					break;
-				case NI_TK_KW_PROCEDURE:
-					ni_read_procedure_constant(lexer,
-					                           &const_proc_label_val,
-					                           &const_proc_num_locals_val,
-					                           &error);
-					if (!n_error_ok(&error)) goto handle_error;
-					/* TODO: Do something with the procedure data. */
-					free(const_proc_label_val);
-					break;
-				case NI_TK_KW_INT32:
-					ni_read_int32_constant(lexer, &const_int32_val, &error);
-					if (!n_error_ok(&error)) goto handle_error;
-					/* TODO: Do something with the int32. */
-					break;
-				default:
-					/* TODO: Create error: expected constant descriptor keyword,
-					 * got token %s. */
-					goto handle_error;
-					break;
-			}
-			if (!n_error_ok(&error)) goto handle_error;
-			next_token = ni_lexer_peek(lexer);
-		}
-	}
-	if (next_token != NI_TK_KW_CODE) {
-		/* TODO: Create error, expected '.code', found something else. */
-	}
-
-	while (next_token != NI_TK_EOF) {
-		switch(next_token) {
-			case NI_TK_OP_RETURN:
-				break;
-			case NI_TK_OP_MOVE:
-				break;
-			case NI_TK_OP_GLOBAL_REF:
-				break;
-			case NI_TK_OP_GLOBAL_SET:
-				break;
-			case NI_TK_OP_JUMP:
-				break;
-			case NI_TK_OP_JUMP_IF:
-				break;
-			case NI_TK_OP_EQ: /* fall-through */
-			case NI_TK_OP_LT: /* fall-through */
-			case NI_TK_OP_LE: /* fall-through */
-			case NI_TK_OP_GT: /* fall-through */
-			case NI_TK_OP_GE: /* fall-through */
-				break;
-			case NI_TK_OP_OR:  /* fall-through */
-			case NI_TK_OP_AND: /* fall-through */
-			case NI_TK_OP_ADD: /* fall-through */
-			case NI_TK_OP_SUB: /* fall-through */
-			case NI_TK_OP_MUL: /* fall-through */
-			case NI_TK_OP_DIV: /* fall-through */
-				break;
-			case NI_TK_OP_NOT:
-				break;
-			case NI_TK_OP_LOAD_BOOL:
-				break;
-			default:
-				/* TODO: Create error: expected opcode, got token %s. */
-				goto handle_error;
-				break;
-		}
-		if (!n_error_ok(&error)) goto handle_error;
-		next_token = ni_lexer_peek(lexer);
-	}
-
-
-handle_error:
-	/* TODO: Actually handle error and free resources. */
-	return NULL;
 }
 
+
+TEST(read_version_recognizes_input) {
+	uint8_t major, minor, revision;
+	NError error = N_ERROR_INITIALIZER;
+	NLexer* lexer = WITH_INPUT(".version 5 200 44");
+	ni_read_version(lexer, &major, &minor, &revision, &error);
+	ASSERT_EOK(&error);
+	ASSERT_EQ_I64(major, 5);
+	ASSERT_EQ_I64(minor, 200);
+	ASSERT_EQ_I64(revision, 44);
+}
+
+
+static NLexer*
+WITH_INPUT(const char* input) {
+	LEXER = ni_new_lexer(ni_new_stream_from_string(input));
+	return LEXER;
+}
--- tests/test-suite.h
+++ tests/test-suite.h
@@ -6,6 +6,8 @@
 
 #include <atest.h>
 
+#include "common/errors.h"
+
 #ifndef SUITE_NAME
 #define SUITE_NAME Untitled
 #endif
@@ -21,6 +23,24 @@
 #define EXPECT_MSG(cond, msg) at_expect_msg(at_result, cond, msg)
 #define ASSERT(cond)          at_assert(at_result, cond)
 #define ASSERT_MSG(cond, msg) at_assert_msg(at_result, cond, msg)
+#define ASSERT_MSG_O(cond, msg) at_assert_msg(at_result, cond, msg);
 
 #define MF         at_allocf
+
+#define ASSERT_EQ_I64(value, expected) do {                          \
+	int64_t _value = (int64_t) value;                              \
+	int64_t _expected = (int64_t) expected;                        \
+	ASSERT_MSG_O(_value == _expected,                              \
+		MF("Expression (%s) expected to have value %ld but found " \
+		   "to have value %ld.",                                   \
+		   #value, _expected, _value))                             \
+} while(0)
+
+#define ASSERT_EOK(error_ptr) do {                                  \
+	NError* _error = error_ptr;                                     \
+	ASSERT_MSG_O(n_error_ok(_error),                                \
+		MF("Error (%s) expected to be ok, but the following error " \
+		   "was reported: \"%s\"",                                  \
+		   #error_ptr, n_error_print(_error)))                      \
+} while(0)
 #endif
